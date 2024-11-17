ab790356b4691cf8913089c562c42f22c401cfee
Aniket Ingole <aniketingolenet@gmail.com>
2024-11-17 16:52:16 +0530

    Added user schema with avatar

---
+++ src/schema/user.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+    {
+        email: {
+            type: String,
+            required: [true, "Email is required"],
+            unique: [true, "Email already exists"],
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+        },
+        password: {
+            type: String,
+            required: [true, "password is required"]
+        },
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [
+                /^[a-zA-Z0-9]+$/,
+                "username must contain only letters and numbers"
+            ]
+        },
+        avatar: {
+            type: String
+        }
+    }, { timestamps: true }
+);
+
+userSchema.pre("save", function saveUser(next) {
+    const user = this;
+    user.avatar = `https://robohash.org/${user.username}`;
+    next();
+});
+
+const user = mongoose.model("user", userSchema);
+
+export default user;
+
