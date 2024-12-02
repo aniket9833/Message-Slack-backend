37eb67f9d40cdda2e677b92335d47524b029ee6d
Aniket Ingole <aniketingolenet@gmail.com>
2024-12-02 17:37:08 +0530

    Added message comm through web socket

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import express from 'express';
+import { createServer } from 'http';
 import { StatusCodes } from 'http-status-codes';
+import { Server } from 'socket.io';
 
 import bullServerAdapter from './config/bullBoardConfig.js';
 import connectDB from './config/dbConfig.js';
 import { PORT } from './config/serverConfig.js';
+import messageHandlers from './controllers/messageSocketController.js';
 import apiRouter from './routes/apiRoutes.js';
 
 const app = express();
+
+const server = createServer(app);
+const io = new Server(server);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -19,7 +25,15 @@ app.get('/ping', (req, res) => {
   return res.status(StatusCodes.OK).json({ message: 'pong' });
 });
 
-app.listen(PORT, async () => {
+io.on('connection', (socket) => {
+  // console.log('a user connected', socket.id);
+  // socket.on('messageFromClient', (data) => {
+  //   console.log('Message from client', data);
+  //   io.emit('new message', data.toUpperCase());
+  // });
+  messageHandlers(io, socket);
+});
+server.listen(PORT, async () => {
   console.log(`Server is running on port ${PORT}`);
   connectDB();
 });
--- src/services/messageService.js
+++ src/services/messageService.js
@@ -1,7 +1,7 @@
 import { StatusCodes } from 'http-status-codes';
 
-import channelRepository from '../repositories/channelRepostiory.js';
-import messageRepository from '../repositories/messageRepository';
+import channelRepository from '../repositories/channelRepository.js';
+import messageRepository from '../repositories/messageRepository.js';
 import ClientError from '../utils/errors/clientError.js';
 import { isUserMemberOfWorkspace } from './workspaceService.js';
 
@@ -25,4 +25,9 @@ export const getMessagesService = async (messageParams, page, limit, user) => {
     limit
   );
   return messages;
+};
+
+export const createMessageService = async (message) => {
+  const newMessage = await messageRepository.create(message);
+  return newMessage;
 };
