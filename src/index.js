8a72f5659209d1e6e2b2834da490c846abfa7ca8
Aniket Ingole <aniketingolenet@gmail.com>
2024-12-02 17:55:15 +0530

    Modified message comm

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import { Server } from 'socket.io';
 import bullServerAdapter from './config/bullBoardConfig.js';
 import connectDB from './config/dbConfig.js';
 import { PORT } from './config/serverConfig.js';
-import messageHandlers from './controllers/messageSocketController.js';
+import ChannelSocketHandlers from './controllers/channelSocketController.js';
+import MessageSocketHandlers from './controllers/messageSocketController.js';
 import apiRouter from './routes/apiRoutes.js';
 
 const app = express();
@@ -31,7 +32,8 @@ io.on('connection', (socket) => {
   //   console.log('Message from client', data);
   //   io.emit('new message', data.toUpperCase());
   // });
-  messageHandlers(io, socket);
+  MessageSocketHandlers(io, socket);
+  ChannelSocketHandlers(io, socket);
 });
 server.listen(PORT, async () => {
   console.log(`Server is running on port ${PORT}`);
