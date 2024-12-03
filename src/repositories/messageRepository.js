01a74f4da4b632023a90e720ce061f0e0c95b1b0
Aniket Ingole <aniketingolenet@gmail.com>
2024-12-03 09:57:35 +0530

    Added messages in channel response

--- src/repositories/messageRepository.js
+++ src/repositories/messageRepository.js
@@ -3,7 +3,7 @@ import crudRepository from './crudRepository.js';
 
 const messageRepository = {
   ...crudRepository(Message),
-  getPaginatedMessaged: async (messageParams, page, limit) => {
+  getPaginatedMessages: async (messageParams, page, limit) => {
     const messages = await Message.find(messageParams)
       .sort({ createdAt: -1 })
       .skip((page - 1) * limit)
